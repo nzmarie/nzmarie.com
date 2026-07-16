dc820d5a1480b46c8d3a84ad23b3f36a71fd8dae
nzmarie <nzmarie.com@gmail.com>
2026-07-17 10:41:39 +1200

    feat: expand suburb options in quick filter for outreach page

--- app/admin/outreach/page.tsx
+++ app/admin/outreach/page.tsx
@@ -746,7 +746,7 @@ export default function OutreachPage() {
             Quick Filter by Suburb
           </label>
           <div style={{ display: "flex", flexWrap: "wrap", gap: "10px", alignItems: "center" }}>
-            {['Northcross', 'Oteha', 'Torbay', 'Fairview Heights', 'Waiake', 'Browns Bay', 'Pinehill', 'Rothesay Bay', 'Murrays Bay', 'Albany'].map((s) => (
+            {['Northcross', 'Oteha', 'Torbay', 'Fairview Heights', 'Waiake', 'Browns Bay', 'Pinehill', 'Rothesay Bay', 'Murrays Bay', 'Albany', 'Long Bay', 'Forrest Hill', 'Schnapper Rock', 'Unsworth Heights', 'Sunnynook', 'Greenhithe', 'Chatswood', 'Mairangi Bay', 'Campbells Bay', 'Castor Bay', 'Milford', 'Glenfield', 'Hillcrest', 'Birkenhead', 'Hauraki'].map((s) => (
               <button
                 key={s}
                 onClick={() => {
